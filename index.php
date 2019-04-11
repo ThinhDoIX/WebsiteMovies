c605c4784e582b8fec9541270d984d253ff74eae
YoruDo <45510642+YoruDo@users.noreply.github.com>
2019-04-11 07:22:48 +0700

    First commit 04/11/2019

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>Movies Homepage| Relax | Enjoy</title>
+
+	<!--Boostrap CSS-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/homepage.css">
+</head>
+<body>
+		<!--Navigation Bar-->
+		<div class="container">
+			
+			<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+				<!--BUTTON TOGGLER-->
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+			    	<span class="navbar-toggler-icon"></span>
+			  	</button>
+
+
+				<div class="collapse navbar-collapse" id="navbarToggler">
+					<a class="navbar-brand text-light" href="index.php">AniMovies</a>
+
+					<!--DROPDOWN MENU-->
+					<div class="dropdown">
+				  		<a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown">
+				    		Thể Loại
+				  		</a>
+
+					  	<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+					    	<a class="dropdown-item" href="#">Action</a>
+					    	<a class="dropdown-item" href="#">Another action</a>
+					    	<a class="dropdown-item" href="#">Something else here</a>
+					  	</div>
+					</div>
+				</div>
+				
+			</nav>
+
+		</div>
+
+
+		<!--BOOTSTRAP JS-->
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
